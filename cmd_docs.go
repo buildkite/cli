6dd25b65ac4f54483eb91f1494bf24b3959e84d8
James Sammut <juma2791@gmail.com>
2023-07-07 14:05:36 +1000

    Added structs for Relevance query

--- cmd_docs.go
+++ cmd_docs.go
@@ -12,8 +12,39 @@ type DocsCommandContext struct {
 	Query     	 string
 }
 
+type payload struct{
+	Params  question `json:"params,omitempty"`
+	Project string   `json:"project,omitempty"`
+}
+
+type question struct {
+	Question string `json:"question,omitempty"`
+}
+
+type Response struct {
+	Status    string    `json:"status"`
+	Errors    []string  `json:"errors"`
+	Output    Output    `json:"output"`
+	Credits   []Credit  `json:"credits_used"`
+	ExecTime  int       `json:"executionTime"`
+	Cost      float64   `json:"cost"`
+}
+
+type Output struct {
+	Answer            string   `json:"answer"`
+	Prompt            string   `json:"prompt"`
+	UserKeyUsed       bool     `json:"user_key_used"`
+	ValidationHistory []string `json:"validation_history"`
+	CreditsCost       float64  `json:"credits_cost"`
+}
+
+type Credit struct {
+	Credits     float64 `json:"credits"`
+	Name        string  `json:"name"`
+	Multiplier  float64 `json:"multiplier,omitempty"`
+	NumUnits    float64 `json:"num_units,omitempty"`
+}
+
 func DocsHelp(ctx DocsCommandContext) error {
 	fmt.Println("hello")
-	
-	return nil
 }
