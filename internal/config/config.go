a89691f43161e063498d03d3514cdd63c71e6c4a
jradtilbrook <jarryd@tilbrooktech.com>
2023-08-19 20:35:39 +0800

    Add configuration file

---
+++ internal/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+const (
+	appData       = "AppData"
+	xdgConfigHome = "XDG_CONFIG_HOME"
+)
+
+// Config path precedence: XDG_CONFIG_HOME, AppData (windows only), HOME.
+func ConfigFile() string {
+	var path string
+	if a := os.Getenv(xdgConfigHome); a != "" {
+		path = filepath.Join(a, "bk.yaml")
+	} else if b := os.Getenv(appData); runtime.GOOS == "windows" && b != "" {
+		path = filepath.Join(b, "Buildkite CLI", "bk.yaml")
+	} else {
+		c, _ := os.UserHomeDir()
+		path = filepath.Join(c, ".config", "bk.yaml")
+	}
+	return path
+}
--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -1,16 +1,24 @@
 package root
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/MakeNowJust/heredoc"
+	"github.com/buildkite/cli/v3/internal/config"
+	authCmd "github.com/buildkite/cli/v3/pkg/cmd/auth"
 	versionCmd "github.com/buildkite/cli/v3/pkg/cmd/version"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func NewCmdRoot(version string) (*cobra.Command, error) {
+	cobra.OnInitialize(initConfig)
+
 	cmd := &cobra.Command{
-		Use: "bk <command> <subcommand> [flags]",
+		Use:   "bk <command> <subcommand> [flags]",
 		Short: "Buildkite CLI",
-		Long: "Work with Buildkite from the command line.",
+		Long:  "Work with Buildkite from the command line.",
 		Example: heredoc.Doc(`
 			$ bk build view
 		`),
@@ -20,6 +28,19 @@ func NewCmdRoot(version string) (*cobra.Command, error) {
 	}
 
 	cmd.AddCommand(versionCmd.NewCmdVersion())
+	cmd.AddCommand(authCmd.NewCmdAuth())
 
 	return cmd, nil
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetConfigFile(config.ConfigFile())
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
