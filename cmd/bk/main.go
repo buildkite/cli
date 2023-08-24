fc2900e4fd822abb3fa6a4a80766613fdeef0f1f
Ben McNicholl <git@benmcnicholl.com>
2023-08-24 15:11:22 +1000

    Use a configure command to set auth

--- cmd/bk/main.go
+++ cmd/bk/main.go
@@ -18,13 +18,18 @@ func main() {
 
 func mainRun() int {
 	ctx := context.Background()
-	viper := viper.New()
-	viper.SetConfigFile(config.ConfigFile())
-	viper.AutomaticEnv()
+	v := viper.New()
+	v.SetConfigFile(config.ConfigFile())
+	v.AutomaticEnv()
 	// attempt to read in config file but it might not exist
-	_ = viper.ReadInConfig()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Print(fmt.Errorf("Unable to find config file at %s: %s", config.ConfigFile(), err))
+			return 1
+		}
+	}
 
-	rootCmd, err := root.NewCmdRoot(viper, build.Version)
+	rootCmd, err := root.NewCmdRoot(v, build.Version)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to create root command: %s\n", err)
 		return 1
