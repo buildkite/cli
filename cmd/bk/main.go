de3287dd70397201aa65debf5a25fb4c8ee249e6
Jarryd Tilbrook <jarryd@buildkite.com>
2023-08-25 15:12:47 +0800

    Revert error checking

--- cmd/bk/main.go
+++ cmd/bk/main.go
@@ -22,12 +22,7 @@ func mainRun() int {
 	v.SetConfigFile(config.ConfigFile())
 	v.AutomaticEnv()
 	// attempt to read in config file but it might not exist
-	if err := v.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			fmt.Print(fmt.Errorf("Unable to find config file at %s: %s", config.ConfigFile(), err))
-			return 1
-		}
-	}
+	_ = v.ReadInConfig()
 
 	rootCmd, err := root.NewCmdRoot(v, build.Version)
 	if err != nil {
