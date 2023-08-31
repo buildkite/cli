10471358894c62fc8fd75b603878788a206a7675
Jarryd Tilbrook <jarryd@buildkite.com>
2023-08-31 11:23:58 +0800

    Improve docs on agent command

--- pkg/cmd/agent/agent.go
+++ pkg/cmd/agent/agent.go
@@ -1,15 +1,27 @@
 package agent
 
 import (
+	"github.com/MakeNowJust/heredoc"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
 	"github.com/spf13/cobra"
 )
 
 func NewCmdAgent(f *factory.Factory) *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "agent <subcommand> [flags]",
-		Short: "",
-		Long:  "",
+		Use:   "agent <command>",
+		Short: "Manage agents",
+		Long:  "Work with Buildkite agents.",
+		Example: heredoc.Doc(`
+			$ bk agent stop buildkite/ffffffff-aaaa-0000-1111111111111111
+		`),
+		Annotations: map[string]string{
+			"help:arguments": heredoc.Doc(`
+				An agent can be supplied as an argument in any of the following formats:
+				- "ORGANIZATION_SLUG/UUID"
+				- "UUID"
+				- by URL, e.g. "https://buildkite.com/organizations/buildkite/agents/ffffffff-aaaa-0000-1111111111111111"
+			`),
+		},
 	}
 
 	cmd.AddCommand(NewCmdAgentStop(f))
