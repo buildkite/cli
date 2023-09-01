54d458ce498ae9ff546fc6b3c3b74cbfd9608f63
Jarryd Tilbrook <jarryd@buildkite.com>
2023-09-01 13:50:29 +0800

    Refactor config for multi orgs

--- pkg/cmd/configure/configure.go
+++ pkg/cmd/configure/configure.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/AlecAivazis/survey/v2"
-	"github.com/buildkite/cli/v3/internal/config"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
 	"github.com/spf13/cobra"
 )
@@ -13,30 +12,43 @@ func NewCmdConfigure(f *factory.Factory) *cobra.Command {
 	var force bool
 
 	cmd := &cobra.Command{
-		Use:   "configure",
-		Args:  cobra.NoArgs,
-		Short: "Configure Buildkite API token",
+		Use:     "configure",
+		Aliases: []string{"config"},
+		Args:    cobra.NoArgs,
+		Short:   "Configure Buildkite API token",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// if the token already exists and --force is not used
-			if !force && f.Config.IsSet(config.APITokenConfigKey) {
+			if !force && f.Config.APIToken != "" {
 				return errors.New("API token already configured. You must use --force.")
 			}
 
-			var token string
-			prompt := &survey.Password{
-				Message: "Paste your API token:",
+			qs := []*survey.Question{
+				{
+					Name:     "org",
+					Prompt:   &survey.Input{Message: "What is your organization slug?"},
+					Validate: survey.Required,
+				},
+				{
+					Name:     "token",
+					Prompt:   &survey.Password{Message: "Paste your API token:"},
+					Validate: survey.Required,
+				},
 			}
+			answers := struct{ Org, Token string }{}
 
-			err := survey.AskOne(prompt, &token)
+			err := survey.Ask(qs, &answers)
 			if err != nil {
 				return err
 			}
 
-			f.Config.Set(config.APITokenConfigKey, token)
-			return f.Config.WriteConfig()
+			f.Config.APIToken = answers.Token
+			f.Config.Organization = answers.Org
+
+			return f.Config.Save()
 		},
 	}
 
 	cmd.Flags().BoolVar(&force, "force", false, "Force setting a new token")
+
 	return cmd
 }
