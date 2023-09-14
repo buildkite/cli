ee8e3fb653a62758c769893bd2a17069c73dbf6f
Ben McNicholl <git@benmcnicholl.com>
2023-09-15 09:22:57 +1000

    fmt

--- pkg/cmd/agent/agent.go
+++ pkg/cmd/agent/agent.go
@@ -31,7 +31,7 @@ func NewCmdAgent(f *factory.Factory) *cobra.Command {
 	}
 
 	cmd.AddCommand(NewCmdAgentStop(f))
-    cmd.AddCommand(NewCmdAgentList(f))
+	cmd.AddCommand(NewCmdAgentList(f))
 
 	return &cmd
 }
--- pkg/cmd/agent/list.go
+++ pkg/cmd/agent/list.go
@@ -1,16 +1,16 @@
 package agent
 
 import (
+	"fmt"
 	"github.com/MakeNowJust/heredoc"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
 	"github.com/buildkite/go-buildkite/v3/buildkite"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
-    "fmt"
-    "github.com/jedib0t/go-pretty/v6/table"
 )
 
 func NewCmdAgentList(f *factory.Factory) *cobra.Command {
-    t := table.NewWriter()
+	t := table.NewWriter()
 	cmd := cobra.Command{
 		DisableFlagsInUseLine: true,
 		Use:                   "list",
@@ -20,15 +20,15 @@ func NewCmdAgentList(f *factory.Factory) *cobra.Command {
             Command to list all agents for the current organization.
 		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-            agents, _, err := f.RestAPIClient.Agents.List(f.Config.Organization, &buildkite.AgentListOptions{})
-            if err != nil {
-                return err
-            }
-            t.AppendHeader(table.Row{"#", "Name", "ID", "State"})
-            for i, agent := range agents {
-                t.AppendRow(table.Row{i + 1, *agent.Name, *agent.ID, *agent.ConnectedState})
-            }
-            fmt.Println(t.Render())
+			agents, _, err := f.RestAPIClient.Agents.List(f.Config.Organization, &buildkite.AgentListOptions{})
+			if err != nil {
+				return err
+			}
+			t.AppendHeader(table.Row{"#", "Name", "ID", "State"})
+			for i, agent := range agents {
+				t.AppendRow(table.Row{i + 1, *agent.Name, *agent.ID, *agent.ConnectedState})
+			}
+			fmt.Println(t.Render())
 			return err
 		},
 	}
