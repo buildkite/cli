304b702390b7c8473ca8dc17a4d0633611743e35
Jarryd Tilbrook <jarryd@buildkite.com>
2023-12-20 10:51:52 +0800

    SUP-1567 Add bk agent list (#175)
    * Add start of `bk agent list`
    
    * Add table output for `bk agent list`
    
    * go mod tidy

---
+++ pkg/cmd/agent/list.go
@@ -0,0 +1,77 @@
+package agent
+
+import (
+	"strings"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/buildkite/cli/v3/internal/io"
+	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdAgentList(f *factory.Factory) *cobra.Command {
+	cmd := cobra.Command{
+		DisableFlagsInUseLine: true,
+		Use:                   "list",
+		Args:                  cobra.NoArgs,
+		Short:                 "List agents",
+		Long: heredoc.Doc(`
+			List all connected agents for the current organization.
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l := io.NewPendingCommand(func() tea.Msg {
+				agents, _, err := f.RestAPIClient.Agents.List(f.Config.Organization, nil)
+				if err != nil {
+					return err
+				}
+
+				if len(agents) == 0 {
+					return io.PendingOutput("No connected agents")
+				}
+
+				var rows []table.Row
+				maxID, maxName, maxStatus, maxTags := 0, 0, 0, 0
+				for _, agent := range agents {
+					if max := len(*agent.ID); max > maxID {
+						maxID = max
+					}
+					if max := len(*agent.Name); max > maxName {
+						maxName = max
+					}
+					if max := len(*agent.ConnectedState); max > maxStatus {
+						maxStatus = max
+					}
+					tags := strings.Join(agent.Metadata, ", ")
+					if max := len(tags); max > maxTags {
+						maxTags = max
+					}
+					row := []string{
+						*agent.ID, *agent.Name, *agent.ConnectedState, tags,
+					}
+
+					rows = append(rows, row)
+				}
+				columns := []table.Column{
+					{Title: "ID", Width: maxID},
+					{Title: "Name", Width: maxName},
+					{Title: "Status", Width: maxStatus},
+					{Title: "Tags", Width: maxTags},
+				}
+				// set the selected style to default
+				styles := table.DefaultStyles()
+				styles.Selected = lipgloss.NewStyle()
+				table := table.New(table.WithColumns(columns), table.WithRows(rows), table.WithHeight(len(rows)), table.WithStyles(styles))
+
+				return io.PendingOutput(table.View())
+			}, "Loading agents")
+
+			_, err := tea.NewProgram(l).Run()
+			return err
+		},
+	}
+
+	return &cmd
+}
