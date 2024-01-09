0596af3258f5dd45dd8dd0f8def8a727d55ecf41
lizrabuya <115472349+lizrabuya@users.noreply.github.com>
2024-01-09 15:48:54 +1100

    SUP-1560 Add bk build new (#183)
    * Initial work on bk build new
    
    * Add tests for parsing pipeline args
    
    * Support creating a build with a pipeline name as argument
    
    * Update docs for bk build --help
    
    * Use cobra's ExactArgs to validate number of arguments

---
+++ pkg/cmd/build/build.go
@@ -0,0 +1,60 @@
+package build
+
+import (
+	"net/url"
+	"strings"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/buildkite/cli/v3/internal/config"
+	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/buildkite/cli/v3/pkg/cmd/validation"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdBuild(f *factory.Factory) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "build <command>",
+		Short: "Manage pipeline builds",
+		Long:  "Work with Buildkite pipeline builds.",
+		Example: heredoc.Doc(`
+			# To create a new build
+			$ bk build new -m "Build from cli" -c "HEAD" -b "main" 
+		`),
+		PersistentPreRunE: validation.CheckValidConfiguration(f.Config),
+		Annotations: map[string]string{
+			"help:arguments": heredoc.Doc(`
+				A pipeline is passed as an argument. It can be supplied in any of the following formats:
+				- "PIPELINE_SLUG"
+				- "ORGANIZATION_SLUG/PIPELINE_SLUG" 
+				- by URL, e.g. "https://buildkite.com/buildkite/buildkite-cli"
+			`),
+		},
+	}
+
+	cmd.AddCommand(NewCmdBuildNew(f))
+
+	return &cmd
+}
+
+func parsePipelineArg(arg string, conf *config.Config) (string, string) {
+	var org, pipeline string
+	pipelineIsURL := strings.Contains(arg, ":")
+	pipelineIsSlug := !pipelineIsURL && strings.Contains(arg, "/")
+
+	if pipelineIsURL {
+		url, err := url.Parse(arg)
+		if err != nil {
+			return "", ""
+		}
+		// eg: url.Path = /buildkite/buildkite-cli
+		part := strings.Split(url.Path, "/")
+		org, pipeline = part[1], part[2]
+	} else if pipelineIsSlug {
+		part := strings.Split(arg, "/")
+		org, pipeline = part[0], part[1]
+	} else {
+		org = conf.Organization
+		pipeline = arg
+	}
+	return org, pipeline
+}
---
+++ pkg/cmd/build/new.go
@@ -0,0 +1,82 @@
+package build
+
+import (
+	"fmt"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/buildkite/cli/v3/internal/io"
+	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/buildkite/go-buildkite/v3/buildkite"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/pkg/browser"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdBuildNew(f *factory.Factory) *cobra.Command {
+	var message string
+	var commit string
+	var branch string
+	var web bool
+
+	cmd := cobra.Command{
+		DisableFlagsInUseLine: true,
+		Use:                   "new <pipeline> [flags]",
+		Short:                 "Creates a new pipeline build",
+		Args:                  cobra.ExactArgs(1),
+		Long: heredoc.Doc(`
+			Creates a new build for the specified pipeline and output the URL to the build.
+
+			It accepts {pipeline_slug}, {org_slug}/{pipeline_slug} or a full URL to the pipeline as an argument.
+		`),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			org, pipeline := parsePipelineArg(args[0], f.Config)
+			return newBuild(org, pipeline, f, message, commit, branch, web)
+		},
+	}
+
+	cmd.Flags().StringVarP(&message, "message", "m", "", "Description of the build. If left blank, the commit message will be used once the build starts.")
+	cmd.Flags().StringVarP(&commit, "commit", "c", "HEAD", "The commit to build.")
+	cmd.Flags().StringVarP(&branch, "branch", "b", "", "The branch to build. Defaults to the default branch of the pipeline.")
+	cmd.Flags().BoolVarP(&web, "web", "w", false, "Open the build in a web browser after it has been created.")
+	cmd.Flags().SortFlags = false
+	return &cmd
+}
+
+func newBuild(org string, pipeline string, f *factory.Factory, message string, commit string, branch string, web bool) error {
+	buildUrl := fmt.Sprintf("https://buildkite.com/%s/%s/builds", org, pipeline)
+	l := io.NewPendingCommand(func() tea.Msg {
+
+		if len(branch) == 0 {
+			p, _, err := f.RestAPIClient.Pipelines.Get(org, pipeline)
+			if err != nil {
+				return err
+			}
+			branch = *p.DefaultBranch
+		}
+
+		newBuild := buildkite.CreateBuild{
+			Message: message,
+			Commit:  commit,
+			Branch:  branch,
+		}
+
+		build, _, err := f.RestAPIClient.Builds.Create(org, pipeline, &newBuild)
+		if err != nil {
+			return err
+		}
+		buildUrl = fmt.Sprintf("%s/%d", buildUrl, *build.Number)
+
+		if web {
+			fmt.Printf("Opening %s in your browser\n", buildUrl)
+			err = browser.OpenURL(buildUrl)
+			if err != nil {
+				fmt.Println("Error opening browser: ", err)
+			}
+		}
+
+		return io.PendingOutput(fmt.Sprintf("Build created: %s", buildUrl))
+	}, fmt.Sprintf("Starting new build for %s", pipeline))
+	p := tea.NewProgram(l)
+	_, err := p.Run()
+	return err
+}
