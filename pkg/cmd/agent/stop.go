7ade369864ea90e639eaed27d5125b37baaa1257
Jarryd Tilbrook <jarryd@buildkite.com>
2024-01-10 14:29:46 +0800

    Change agent stop to use StoppableAgent model

--- pkg/cmd/agent/stop.go
+++ pkg/cmd/agent/stop.go
@@ -1,11 +1,8 @@
 package agent
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/MakeNowJust/heredoc"
-	"github.com/buildkite/cli/v3/internal/io"
+	"github.com/buildkite/cli/v3/internal/agent"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
@@ -18,50 +15,43 @@ func NewCmdAgentStop(f *factory.Factory) *cobra.Command {
 		DisableFlagsInUseLine: true,
 		Use:                   "stop <agent> [--force]",
 		Args:                  cobra.MinimumNArgs(1),
-		Short:                 "Stop an agent",
+		Short:                 "Stop Buildkite agents",
 		Long: heredoc.Doc(`
-			Instruct an agent to stop accepting new build jobs and shut itself down.
+			Instruct one or more agents to stop accepting new build jobs and shut itself down.
 
 			If the "ORGANIZATION_SLUG/" portion of the "ORGANIZATION_SLUG/UUID" agent argument
 			is omitted, it uses the currently selected organization.
 
-			If the agent is already stopped the command returns an error.
+			The --force flag applies to all agents that are stopped.
 		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			switch agents := len(args); {
-			case agents >= 1:
-				// Construct an agentStopErrors variable to construct errors
-				var agentStopErrors error
-				for _, agent := range args {
-					err := stopAgent(agent, f, force)
-					// Append to agentStopErrors if there was an error stopping an agent
-					if err != nil {
-						agentStopErrors = errors.Join(agentStopErrors, err)
-					}
+			model := agent.NewStoppableAgent(args[0], func() agent.StatusUpdate {
+				org, agentID := parseAgentArg(args[0], f.Config)
+
+				return agent.StatusUpdate{
+					Status: agent.Stopping,
+					Cmd: func() tea.Msg {
+						_, err := f.RestAPIClient.Agents.Stop(org, agentID, force)
+						if err != nil {
+							return agent.StatusUpdate{
+								Err: err,
+								Cmd: tea.Quit,
+							}
+						}
+						return agent.StatusUpdate{
+							Status: agent.Succeeded,
+							Cmd:    tea.Quit,
+						}
+					},
 				}
-				return agentStopErrors
-			}
-			return nil
+			})
+			p := tea.NewProgram(model)
+			_, err := p.Run()
+			return err
 		},
 	}
 
 	cmd.Flags().BoolVar(&force, "force", false, "Force stop the agent. Terminating any jobs in progress")
 
 	return &cmd
-}
-
-func stopAgent(agent string, f *factory.Factory, force bool) error {
-	l := io.NewPendingCommand(func() tea.Msg {
-		org, agent := parseAgentArg(agent, f.Config)
-		_, err := f.RestAPIClient.Agents.Stop(org, agent, force)
-		if err != nil {
-			return err
-		}
-		return io.PendingOutput(fmt.Sprintf("Stopped agent %s", agent))
-	}, fmt.Sprintf("Stopping agent %s", agent))
-
-	p := tea.NewProgram(l)
-	_, err := p.Run()
-
-	return err
 }
