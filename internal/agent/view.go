66e6a835e1486875cf30312afe3e7707c84a9c7b
James Sammut <juma2791@gmail.com>
2024-01-16 17:10:02 +1100

    Logic adjustment for metadata tag parsing

--- internal/agent/view.go
+++ internal/agent/view.go
@@ -8,14 +8,20 @@ import (
 func ParseMetadata(metadataList []string) (string, string) {
 	var metadata, queue string
 
-	for _, v := range metadataList {
-		metadataStr := strings.Split(v, "=")
-		if metadataStr[0] == "queue" {
-			queue = metadataStr[1]
-		} else {
-			metadata += fmt.Sprintf("%s\n", v)
+	// If no tags/only queue name (or default) is set - return a tilde (~) representing
+	// no metadata key/value tags, along with the found queue name 
+	if len(metadataList) == 1 {
+		return "~", strings.Split(metadataList[0], "=")[1]
+	} else {
+		// We can't guarantee order of metadata key/value pairs, extract each pair
+		// and the queue name when found in the respective element string
+		for _, v := range metadataList {
+			if strings.Contains(v, "queue=") {
+				queue = strings.Split(v, "=")[1]
+			} else {
+				metadata += fmt.Sprintf("%s\n", v)
+			}
 		}
-	}
-
-	return metadata, queue
+		return metadata, queue
+	}	
 }
