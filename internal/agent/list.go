a0943e36815be891eea42906b41ae695331475ed
Jarryd Tilbrook <jarryd@buildkite.com>
2024-01-17 07:56:40 +0800

    SUP-1699 Change styling of `bk agent list` (#186)
    * Split out agentListItem to separate file
    
    * Remove separate agents field
    
    * Set default queue as "default"
    
    * Add separate ItemDelegate for rendering items
    
    * Use bubbletea alt screen
    
    * Move API call to tea.Cmd
    
    * Display errors to the user
    
    * Refactor to new function

--- internal/agent/list.go
+++ internal/agent/list.go
@@ -2,9 +2,8 @@ package agent
 
 import (
 	"fmt"
+	"time"
 
-	"github.com/buildkite/cli/v3/pkg/cmd/factory"
-	"github.com/buildkite/go-buildkite/v3/buildkite"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -12,83 +11,53 @@ import (
 
 var agentListStyle = lipgloss.NewStyle().Margin(1, 2)
 
-type agentListItem struct {
-	title, metadata string
+type AgentListModel struct {
+	agentList   list.Model
+	agentLoader tea.Cmd
 }
 
-func (ali agentListItem) Title() string       { return ali.title }
-func (ali agentListItem) Description() string { return ali.metadata }
-func (ali agentListItem) FilterValue() string { return ali.title }
+func NewAgentList(loader tea.Cmd) AgentListModel {
+	l := list.New(nil, NewDelegate(), 0, 0)
+	l.Title = "Buildkite Agents"
 
-type agentListModel struct {
-	agentList list.Model
-	agents    []buildkite.Agent
-	quitting  bool
+	return AgentListModel{
+		agentList:   l,
+		agentLoader: loader,
+	}
 }
 
-func ObtainAgents(f *factory.Factory, name, version, hostname string) (*agentListModel, error) {
-	var alo buildkite.AgentListOptions
-	var items []list.Item
-
-	if name != "" || version != "" || hostname != "" {
-		alo = buildkite.AgentListOptions{
-			Name:     name,
-			Version:  version,
-			Hostname: hostname,
-		}
-	}
-
-	// Obtain agent list
-	agents, _, err := f.RestAPIClient.Agents.List(f.Config.Organization, &alo)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, agent := range agents {
-		items = append(items, agentListItem{
-			title:    *agent.Name,
-			metadata: fmt.Sprintf("%s | v%s | %s", *agent.ID, *agent.Version, *agent.ConnectedState),
-		})
-	}
-
-	m := agentListModel{
-		agentList: list.New(items, list.NewDefaultDelegate(), 20, 0),
-		agents:    agents,
-	}
-
-	// Set Title
-	m.agentList.Title = "Buildkite Agents"
-
-	return &m, nil
+func (m AgentListModel) Init() tea.Cmd {
+	return m.agentLoader
 }
 
-func (m agentListModel) Init() tea.Cmd {
-	return nil
-}
-
-func (m agentListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m AgentListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-		}
 	case tea.WindowSizeMsg:
+		// when viewport size is reported, start a spinner and show a message to the user indicating agents are loading
 		h, v := agentListStyle.GetFrameSize()
 		m.agentList.SetSize(msg.Width-h, msg.Height-v)
+		return m, tea.Batch(m.agentList.StartSpinner(), m.agentList.NewStatusMessage("Loading agents"))
+	case NewAgentItemsMsg:
+		// when a new page of agents is received, append them to existing agents in the list and stop the loading
+		// spinner
+		allItems := append(m.agentList.Items(), msg.Items()...)
+		cmds = append(cmds, m.agentList.SetItems(allItems))
+		m.agentList.StopSpinner()
+	case error:
+		m.agentList.StopSpinner()
+		// show a status message for a long time
+		m.agentList.StatusMessageLifetime = time.Duration(time.Hour)
+		return m, m.agentList.NewStatusMessage(fmt.Sprintf("Failed loading agents: %s", msg.Error()))
 	}
 
 	var cmd tea.Cmd
 	m.agentList, cmd = m.agentList.Update(msg)
-	return m, cmd
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
-func (m agentListModel) View() string {
-	if m.quitting {
-		return ""
-	} else {
-		return agentListStyle.Render(m.agentList.View())
-	}
+func (m AgentListModel) View() string {
+	return agentListStyle.Render(m.agentList.View())
 }
