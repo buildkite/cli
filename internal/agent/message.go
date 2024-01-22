d879ee3bb3cfd7dff25c59c06cdc2770db6a580b
Jarryd Tilbrook <jarryd@buildkite.com>
2024-01-22 21:16:18 +0800

    Add Cmder interface

--- internal/agent/message.go
+++ internal/agent/message.go
@@ -2,9 +2,22 @@ package agent
 
 import (
 	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type NewAgentItemsMsg []AgentListItem
+
+// Msger is implemented by any value that can supply a bubbletea Msg.
+// The Msg method is used to pass a Msg to a running bubbletea Program.
+type Msger interface {
+	Msg() tea.Msg
+}
+
+// Cmder is implemented by any value that can supply a bubbletea Cmd.
+// The Cmd method is used to pass a Cmd to a running bubbletea Program.
+type Cmder interface {
+	Cmd() tea.Cmd
+}
 
 func (a NewAgentItemsMsg) Items() []list.Item {
 	agg := make([]list.Item, len(a))
@@ -16,4 +29,9 @@ func (a NewAgentItemsMsg) Items() []list.Item {
 
 type AgentStopped struct {
 	Agent AgentListItem
+	cmd   tea.Cmd
+}
+
+func (a AgentStopped) Cmd() tea.Cmd {
+	return a.cmd
 }
