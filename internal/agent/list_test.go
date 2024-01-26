9a38455d6322324627c8b14096d62e91bcbead19
Jarryd Tilbrook <jarryd@buildkite.com>
2024-01-26 14:10:43 +0800

    Fix test after merge

--- internal/agent/list_test.go
+++ internal/agent/list_test.go
@@ -2,10 +2,12 @@ package agent
 
 import (
 	"io"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/buildkite/go-buildkite/v3/buildkite"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/x/exp/teatest"
@@ -16,30 +18,43 @@ func init() {
 	lipgloss.SetColorProfile(termenv.Ascii)
 }
 
+func simpleAgentLoader(wg *sync.WaitGroup) func(int) tea.Cmd {
+	return func(int) tea.Cmd {
+		return func() tea.Msg {
+			wg.Done()
+			return NewAgentItemsMsg(
+				[]AgentListItem{
+					{Agent: &buildkite.Agent{Name: buildkite.String("test agent"), ConnectedState: buildkite.String("connected"), Version: buildkite.String("0.0.0")}},
+					{Agent: &buildkite.Agent{Name: buildkite.String("test agent 2"), ConnectedState: buildkite.String("connected"), Version: buildkite.String("0.0.0")}},
+				},
+				1,
+			)
+		}
+	}
+}
+
 func TestAgentListModel(t *testing.T) {
 	t.Parallel()
 
 	t.Run("agents are added and rendered", func(t *testing.T) {
-		model := NewAgentList(func() tea.Msg {
-			return NewAgentItemsMsg{
-				AgentListItem{Agent: &buildkite.Agent{Name: buildkite.String("test agent"), ConnectedState: buildkite.String("connected"), Version: buildkite.String("0.0.0")}},
-			}
-		}, func(s string, b bool) any { return nil })
+		var wg sync.WaitGroup
+		wg.Add(1)
+		model := NewAgentList(simpleAgentLoader(&wg), 1, 1, func(s string, b bool) any { return nil })
 
-		testModel := teatest.NewTestModel(t, model)
-		timer := time.NewTimer(time.Millisecond * 100)
-		go (func() {
-			<-timer.C
-			testModel.Send(tea.Quit())
-		})()
+		// disable spinner for testing (it causes flaky time-sensitive results)
+		model.agentList.SetSpinner(spinner.Spinner{Frames: []string{}})
+		testModel := teatest.NewTestModel(t, model, teatest.WithInitialTermSize(100, 100))
 
+		// wait on the wait group to add an agent and then quit
+		wg.Wait()
+		testModel.Type("q")
 		finalModel := testModel.FinalModel(t, teatest.WithFinalTimeout(time.Second))
 		finalOutput, err := io.ReadAll(testModel.FinalOutput(t))
 		if err != nil {
 			t.Error(err)
 		}
 
-		if len(finalModel.(AgentListModel).agentList.Items()) != 1 {
+		if len(finalModel.(AgentListModel).agentList.Items()) != 2 {
 			t.Error("model does not have an agent")
 		}
 		teatest.RequireEqualOutput(t, finalOutput)
