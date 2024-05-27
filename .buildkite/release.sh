73adc5e7b5adacb71e9d44a1b8f690fcbb2ef346
Jarryd Tilbrook <jarryd@buildkite.com>
2024-05-27 11:54:28 +0800

    Add test release steps to CI

---
+++ .buildkite/release.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+
+# NOTE: do not exit on non-zero returns codes
+set -uo pipefail
+
+AUDIENCE=$1
+
+# grab a token for pushing packages to buildkite with an expiry of 3 mins
+TOKEN=$(buildkite-agent oidc request-token --audience "$AUDIENCE" --lifetime 180)
+
+if [ ! $? ]; then
+    echo "Failed to retrieve OIDC token"
+    exit 1
+fi
+
+goreleaser release --clean --auto-snapshot --skip publish
+
+if [ ! $? ]; then
+    echo "Failed to build a release"
+    exit 1
+fi
+
+for FILE in dist/*.rpm; do
+    echo curl -X POST https://api.buildkite.com/v2/packages/organizations/jradtilbrook/registries/cli-rpm/packages \
+         -H "Authorization: Bearer ${TOKEN}" \
+         -F "file=@$FILE"
+    if [ ! $? ]; then
+        echo "Failed to push RPM package"
+        exit 1
+    fi
+done
