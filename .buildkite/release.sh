07cdc29e7a92a7fc776cc869e7e68f56a0921caf
Jarryd Tilbrook <jarryd@buildkite.com>
2024-05-27 15:53:41 +0800

    Modularise release script

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -1,16 +1,71 @@
 #!/bin/env bash
 
+#
+# This script is used to build a release of the CLI and publish it to multiple registries on Buildkite
+#
+
 # NOTE: do not exit on non-zero returns codes
 set -uo pipefail
 
-AUDIENCE=$1
+GORELEASER_ARGS=""
+ORGANIZATION=""
+REGISTRY=""
+PUBLISH=false
+PACKAGE=""
 
-goreleaser release --clean --snapshot
+function audience() {
+    ORG=$1
+    REGISTRY=$2
+    echo "https://packages.buildkite.com/organizations/${ORG}/packages/registries/${REGISTRY}"
+}
+function upload_url() {
+    ORG=$1
+    REGISTRY=$2
+    echo "https://api.buildkite.com/v2/packages/organizations/${ORG}/registries/${REGISTRY}/packages"
+}
+
+# Parse flags and their arguments
+while shopt -s nullglob; do
+    case "$1" in
+    --snapshot)
+        GORELEASER_ARGS="${GORELEASER_ARGS} --snapshot"
+        shift
+        ;;
+    --org)
+        ORGANIZATION=$2
+        shift 2
+        ;;
+    --registry)
+        REGISTRY=$2
+        shift 2
+        ;;
+    --package)
+        PACKAGE=$2
+        shift 2
+        ;;
+    --publish)
+        PUBLISH=true
+        shift
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
+goreleaser release --clean ${GORELEASER_ARGS}
 
 if [ ! $? ]; then
     echo "Failed to build a release"
     exit 1
 fi
+
+if [ ! $PUBLISH ]; then
+    echo "Not publishing package to registries"
+    exit 0
+fi
+
+AUDIENCE=$(audience $ORGANIZATION $REGISTRY)
 
 # grab a token for pushing packages to buildkite with an expiry of 3 mins
 TOKEN=$(buildkite-agent oidc request-token --audience "$AUDIENCE" --lifetime 180)
@@ -20,10 +75,8 @@ if [ ! $? ]; then
     exit 1
 fi
 
-echo $TOKEN
-
-for FILE in dist/*.rpm; do
-    curl -X POST https://api.buildkite.com/v2/packages/organizations/jradtilbrook/registries/cli-rpm/packages \
+for FILE in dist/*.${PACKAGE}; do
+    curl -X POST $(upload_url $ORGANIZATION $REGISTRY) \
          -H "Authorization: Bearer ${TOKEN}" \
          -F "file=@${FILE}" \
         --fail-with-body
