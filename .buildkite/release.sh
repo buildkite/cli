482cd7d3e23a26e30528cb4d4ca200099436e157
Jarryd Tilbrook <jarryd@buildkite.com>
2024-06-04 11:48:11 +0800

    Add NFPM to goreleaser (#260)
    * Add linux package output from goreleaser
    
    * Remove bin prefix from build targets
    
    * Consolidate docker-compose for release steps
    
    * Move command to plugin config
    
    * Add test release steps to CI
    
    * Add Dockerfile for goreleaser
    
    This is so we can leverage dependabot to update the goreleaser version automatically for us.
    
    * Move to custom image for newest goreleaser version
    
    * Reorder release script
    
    * Force snapshot mode
    
    * Remove echo
    
    * Fail curl command if response is not 200
    
    * Debug
    
    * Fix curl command
    
    * Modularise release script
    
    * Add bash for release script
    
    * Typo
    
    * Fix bash functions
    
    * Revert "Add bash for release script"
    
    This reverts commit e2c5d3daa0e35741b701b90d8551101766e5c8ad.
    
    * Simplify release using env vars
    
    * Remove quote
    
    * Fix goreleaser build failures
    
    * Fix error checking
    
    * Remove comments in goreleaser
    
    * Switch to goreleaser pro
    
    * Add job api socket to container
    
    * Mount job socket to container
    
    * Add matrix to build step
    
    * Split goreleaser builds
    
    * Expand more matrix targets
    
    * Simplify matrix to just OS
    
    ---------
    
    Co-authored-by: lizrabuya <115472349+lizrabuya@users.noreply.github.com>

---
+++ .buildkite/release.sh
@@ -0,0 +1,62 @@
+#!/bin/env bash
+
+#
+# This script is used to build a release of the CLI and publish it to multiple registries on Buildkite
+#
+
+# NOTE: do not exit on non-zero returns codes
+set -uo pipefail
+
+# should we publish the build packages
+PUBLISH=${PUBLISH:-false}
+GORELEASER_ARGS=${GORELEASER_ARGS:-""}
+
+audience() {
+    ORG=$1
+    REGISTRY=$2
+    echo "https://packages.buildkite.com/organizations/${ORG}/packages/registries/${REGISTRY}"
+}
+upload_url() {
+    ORG=$1
+    REGISTRY=$2
+    echo "https://api.buildkite.com/v2/packages/organizations/${ORG}/registries/${REGISTRY}/packages"
+}
+
+GORELEASER_KEY=$(buildkite-agent secret get goreleaser_key)
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to retrieve GoReleaser Pro key"
+    exit 1
+fi
+
+if ! goreleaser release --clean ${GORELEASER_ARGS}; then
+    echo "Failed to build a release"
+    exit 1
+fi
+
+if [[ $PUBLISH == false ]]; then
+    echo "Not publishing package to registries"
+    exit 0
+fi
+
+AUDIENCE=$(audience $ORGANIZATION $REGISTRY)
+
+# grab a token for pushing packages to buildkite with an expiry of 3 mins
+TOKEN=$(buildkite-agent oidc request-token --audience "$AUDIENCE" --lifetime 180)
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to retrieve OIDC token"
+    exit 1
+fi
+
+for FILE in dist/*.${PACKAGE}; do
+    curl -X POST $(upload_url $ORGANIZATION $REGISTRY) \
+         -H "Authorization: Bearer ${TOKEN}" \
+         -F "file=@${FILE}" \
+        --fail-with-body
+
+    if [[ $? -ne 0 ]]; then
+        echo "Failed to push RPM package $file"
+        exit 1
+    fi
+done
