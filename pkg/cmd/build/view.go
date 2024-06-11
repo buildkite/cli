0dc169a059b8a4ff5bab86d9e2f6296629a480a7
lizrabuya <115472349+lizrabuya@users.noreply.github.com>
2024-06-11 16:51:28 +1000

    Use resolved build number instead of the positional args

--- pkg/cmd/build/view.go
+++ pkg/cmd/build/view.go
@@ -57,7 +57,7 @@ func NewCmdBuildView(f *factory.Factory) *cobra.Command {
 			}
 
 			if web {
-				buildUrl := fmt.Sprintf("https://buildkite.com/%s/%s/builds/%s", bld.Organization, bld.Pipeline, args[0])
+				buildUrl := fmt.Sprintf("https://buildkite.com/%s/%s/builds/%d", bld.Organization, bld.Pipeline, bld.BuildNumber)
 				fmt.Printf("Opening %s in your browser\n\n", buildUrl)
 				return browser.OpenURL(buildUrl)
 			}
