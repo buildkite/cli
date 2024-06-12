4f5a57e1ad03309777ac8b3ec655ac45ba5ec1e9
Jarryd Tilbrook <jarryd@buildkite.com>
2024-06-12 10:20:06 +0800

    Bare minimum open ai prompt

--- pkg/cmd/ai/ai.go
+++ pkg/cmd/ai/ai.go
@@ -3,27 +3,23 @@ package ai
 import (
 	"github.com/MakeNowJust/heredoc"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
-	"github.com/buildkite/cli/v3/pkg/cmd/validation"
 	"github.com/spf13/cobra"
 )
 
 func NewCmdAI(f *factory.Factory) *cobra.Command {
 	cmd := cobra.Command{
-		Hidden:  true,
-		Use:     "ai <command>",
-		Short:   "Manage AI integration.",
-		Long:    "Work with Buildkite AI.",
-		PreRunE: validation.OpenAITokenConfigured(f.Config),
+		Hidden: true,
+		Use:    "ai <command>",
+		Short:  "Manage AI integration.",
+		Long:   "Work with Buildkite AI.",
 		Example: heredoc.Doc(`
 			# To configure your AI token
 			$ bk ai configure
 		`),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
-		},
 	}
 
 	cmd.AddCommand(NewCmdAIConfigure(f))
+	cmd.AddCommand(NewCmdAIAsk(f))
 
 	return &cmd
 }
