7ef0ad0140582f1180ab9a0338d81eae95bd7560
Jarryd Tilbrook <jarryd@buildkite.com>
2024-06-12 13:17:21 +0800

    `bk build download --logs` command (#271)
    * Fill out bk build download cmd
    
    * First pass to download job logs
    
    * Add spinner while downloading logs
    
    * Refactor spinner and log messages
    
    * Remove logs flag
    
    * Update to huh? spinner

---
+++ pkg/cmd/build/download.go
@@ -0,0 +1,97 @@
+package build
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/buildkite/cli/v3/internal/build"
+	buildResolver "github.com/buildkite/cli/v3/internal/build/resolver"
+	pipelineResolver "github.com/buildkite/cli/v3/internal/pipeline/resolver"
+	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdBuildDownload(f *factory.Factory) *cobra.Command {
+	cmd := cobra.Command{
+		DisableFlagsInUseLine: true,
+		Use:                   "download [number [pipeline]] [flags]",
+		Short:                 "Download job logs for a build",
+		Long:                  "Download allows you to download resources for a build.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			pipelineRes := pipelineResolver.NewAggregateResolver(
+				pipelineResolver.ResolveFromPositionalArgument(args, 1, f.Config),
+				pipelineResolver.ResolveFromConfig(f.Config, pipelineResolver.PickOne),
+				pipelineResolver.ResolveFromRepository(f, pipelineResolver.CachedPicker(f.Config, pipelineResolver.PickOne)),
+			)
+
+			buildRes := buildResolver.NewAggregateResolver(
+				buildResolver.ResolveFromPositionalArgument(args, 0, pipelineRes.Resolve, f.Config),
+				buildResolver.ResolveBuildFromCurrentBranch(f.GitRepository, pipelineRes.Resolve, f),
+			)
+
+			bld, err := buildRes.Resolve(cmd.Context())
+			if err != nil {
+				return err
+			}
+			if bld == nil {
+				return fmt.Errorf("could not resolve a build")
+			}
+
+			spinErr := spinner.New().
+				Title("Downloading logs").
+				Action(func() {
+					err = download(*bld, f)
+				}).
+				Run()
+			if spinErr != nil {
+				return spinErr
+			}
+
+			return err
+		},
+	}
+
+	return &cmd
+}
+
+func download(build build.Build, f *factory.Factory) error {
+	b, _, err := f.RestAPIClient.Builds.Get(build.Organization, build.Pipeline, fmt.Sprint(build.BuildNumber), nil)
+	if err != nil {
+		return err
+	}
+	if b == nil {
+		return fmt.Errorf("could not find build for %s #%d", build.Pipeline, build.BuildNumber)
+	}
+
+	directory := fmt.Sprintf("build-logs-%s", *b.ID)
+	err = os.MkdirAll(directory, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	for _, job := range b.Jobs {
+		// only script (command) jobs will have logs
+		if job == nil || *job.Type != "script" {
+			continue
+		}
+
+		log, _, err := f.RestAPIClient.Jobs.GetJobLog(build.Organization, build.Pipeline, *b.ID, *job.ID)
+		if err != nil {
+			return err
+		}
+		if log == nil {
+			return fmt.Errorf("could not get logs for job %s", *job.ID)
+		}
+
+		err = os.WriteFile(filepath.Join(directory, *job.ID), []byte(*log.Content), 0644)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("\nDownloaded logs to: %s\n", directory)
+
+	return nil
+}
