50caf0e235161565231db67e77fba6ea33d316d6
Jarryd Tilbrook <jarryd@buildkite.com>
2024-06-17 17:35:20 +0800

    Quieten curl and tar

--- .buildkite/tag.sh
+++ .buildkite/tag.sh
@@ -16,11 +16,10 @@ git tag -l
 # TODO verify the tag format and that its semver newer than the most previous
 
 echo "--- Downloading gh"
-curl -L https://github.com/cli/cli/releases/download/v2.51.0/gh_2.51.0_linux_amd64.tar.gz | tar xvz
+curl -sL https://github.com/cli/cli/releases/download/v2.51.0/gh_2.51.0_linux_amd64.tar.gz | tar xz
 echo "--- Logging in to gh"
 gh_2.51.0_linux_amd64/bin/gh auth setup-git
 
 echo "+++ Tagging ${BUILDKITE_COMMIT} with ${TAG}"
 git tag "${TAG}"
-
 git push origin "${TAG}"
