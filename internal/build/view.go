715c329f7975c7af1e63e2795167dd098b28e782
Ben McNicholl <git@benmcnicholl.com>
2024-06-19 10:15:40 +1000

    Do it better

--- internal/build/view.go
+++ internal/build/view.go
@@ -2,17 +2,17 @@ package build
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/buildkite/go-buildkite/v3/buildkite"
 	"github.com/charmbracelet/lipgloss"
 )
 
-const messageMaxLength = 40
 
 func BuildSummary(build *buildkite.Build) string {
-	message := *build.Message
-	buildInfo := fmt.Sprintf("%s %s %s", renderBuildNumber(*build.State, *build.Number), message[:messageMaxLength], renderBuildState(*build.State, *build.Blocked))
+	message := trimMessage(*build.Message)
+	buildInfo := fmt.Sprintf("%s %s %s", renderBuildNumber(*build.State, *build.Number), message, renderBuildState(*build.State, *build.Blocked))
 
 	source := fmt.Sprintf("Triggered via %s by %s ∘ Created on %s",
 		*build.Source,
@@ -100,4 +100,14 @@ func renderBuildNumber(state string, number int) string {
 	}
 
 	return lipgloss.NewStyle().Foreground(stateColor).Render(fmt.Sprintf("Build #%d", number))
+}
+
+func trimMessage(msg string) string {
+  newlineIndex := strings.Index(msg, "\n")
+	if newlineIndex != -1 {
+		beforeNewline := msg[:newlineIndex]
+		return beforeNewline 
+	} else {
+    return msg
+	}
 }
