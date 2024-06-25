695c4a39bd8aad7f733e886b17616a51054a6524
priya kandababu <priyakandababu@192-168-1-126.tpgi.com.au>
2024-06-25 12:09:03 +1000

    include go routines

--- internal/cluster/query.go
+++ internal/cluster/query.go
@@ -3,6 +3,7 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/buildkite/cli/v3/internal/graphql"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
@@ -27,18 +28,37 @@ func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string
 		Queues:           []Queue{},
 	}
 
+	queueChannel := make(chan Queue, len(q.Organization.Cluster.Queues.Edges))
+	errorChannel := make(chan error, len(q.Organization.Cluster.Queues.Edges))
+	var wg sync.WaitGroup
+
 	for _, edge := range q.Organization.Cluster.Queues.Edges {
-		agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{edge.Node.Id})
-		if err != nil {
-			return nil, fmt.Errorf("unable to read Cluster Queue Agents: %s", err.Error())
+		wg.Add(1)
+		go func(id, key string) {
+			defer wg.Done()
+			agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{id})
+			if err != nil {
+				errorChannel <- fmt.Errorf("unable to read Cluster Queue Agents %s: %s", id, err.Error())
+				return
+			}
 
-		}
+			queue := Queue{
+				Id:           id,
+				Name:         key,
+				ActiveAgents: len(agent.Organization.Agents.Edges),
+			}
+			queueChannel <- queue
+		}(edge.Node.Id, edge.Node.Key)
 
-		queue := Queue{
-			Id:           edge.Node.Id,
-			Name:         edge.Node.Key,
-			ActiveAgents: len(agent.Organization.Agents.Edges),
-		}
+	}
+
+	go func() {
+		wg.Wait()
+		close(queueChannel)
+		close(errorChannel)
+	}()
+
+	for queue := range queueChannel {
 		cluster.Queues = append(cluster.Queues, queue)
 
 	}
