23bd63e0b7830311c9b80e7945389169cc95f151
Jarryd Tilbrook <jarryd@buildkite.com>
2024-07-10 15:11:43 +0800

    Add some helpers around Tool lists

--- internal/ai/tool.go
+++ internal/ai/tool.go
@@ -1,8 +1,31 @@
 package ai
 
-import "github.com/sashabaranov/go-openai"
+import (
+	"github.com/sashabaranov/go-openai"
+)
 
 type Tool interface {
 	ToolDefinition() openai.Tool
 	Execute(string) (any, error)
+}
+
+type EnabledTools []Tool
+
+func (et EnabledTools) GetTool(name string) *Tool {
+	for _, v := range et {
+		if v.ToolDefinition().Function.Name == name {
+			return &v
+		}
+	}
+
+	return nil
+}
+
+func (et EnabledTools) GetDefinitions() []openai.Tool {
+	tools := make([]openai.Tool, len(et))
+	for i, v := range et {
+		tools[i] = v.ToolDefinition()
+	}
+
+	return tools
 }
