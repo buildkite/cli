e20222fd1b2a90e558c1792914a3d79a1514070e
priya kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2024-07-10 15:57:18 +1000

    test err group

--- internal/cluster/query.go
+++ internal/cluster/query.go
@@ -5,21 +5,19 @@ import (
 	"fmt"
 	"sync"
 
+	"golang.org/x/sync/errgroup"
+
 	"github.com/buildkite/cli/v3/internal/graphql"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
 )
 
 func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string, f *factory.Factory) (*Cluster, error) {
-
 	q, err := graphql.GetClusterQueues(ctx, f.GraphQLClient, OrganizationSlug, ClusterID)
-
 	if err != nil {
-		fmt.Println("Unable to read Cluster Queues: ", err.Error())
-		return nil, err
+		return nil, fmt.Errorf("unable to read Cluster Queues: %s", err.Error())
 	}
 
 	ClusterDescription := q.Organization.Cluster.Description
-
 	cluster := Cluster{
 		OrganizationSlug: OrganizationSlug,
 		ClusterID:        ClusterID,
@@ -29,39 +27,43 @@ func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string
 	}
 
 	queueChannel := make(chan Queue, len(q.Organization.Cluster.Queues.Edges))
-	errorChannel := make(chan error, len(q.Organization.Cluster.Queues.Edges))
+
 	var wg sync.WaitGroup
+	var e errgroup.Group
 
 	for _, edge := range q.Organization.Cluster.Queues.Edges {
 		wg.Add(1)
-		go func(id, key string) {
+
+		e.Go(func() error {
 			defer wg.Done()
-			agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{id})
+			agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{edge.Node.Id})
 			if err != nil {
-				errorChannel <- fmt.Errorf("unable to read Cluster Queue Agents %s: %s", id, err.Error())
-				return
+				return fmt.Errorf("unable to read Cluster Queue Agents %s: %s", edge.Node.Id, err.Error())
 			}
 
 			queue := Queue{
-				Id:           id,
-				Name:         key,
+				Id:           edge.Node.Id,
+				Name:         edge.Node.Key,
 				ActiveAgents: len(agent.Organization.Agents.Edges),
 			}
 			queueChannel <- queue
-		}(edge.Node.Id, edge.Node.Key)
+			return nil
 
+		})
 	}
 
 	go func() {
 		wg.Wait()
 		close(queueChannel)
-		close(errorChannel)
 	}()
 
 	for queue := range queueChannel {
 		cluster.Queues = append(cluster.Queues, queue)
-
 	}
-	fmt.Println(errorChannel)
+
+	if err := e.Wait(); err != nil {
+		return nil, fmt.Errorf("error waiting for goroutines: %s", err.Error())
+	}
+
 	return &cluster, nil
 }
