68dccc366de1934bbaa23c7cb048b0aea0742cf7
PriyaSudip <40382124+PriyaSudip@users.noreply.github.com>
2024-07-12 15:06:37 +1000

    include go routines (#304)
    * include go routines
    
    * commit query changes with go routine
    
    * test err group
    
    * Including errgroup with queue
    
    ---------
    
    Co-authored-by: priya kandababu <priyakandababu@192-168-1-126.tpgi.com.au>
    Co-authored-by: priya kandababu <priyakandababu@192-168-1-122.tpgi.com.au>

--- internal/cluster/query.go
+++ internal/cluster/query.go
@@ -6,38 +6,50 @@ import (
 
 	"github.com/buildkite/cli/v3/internal/graphql"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"golang.org/x/sync/errgroup"
 )
 
 func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string, f *factory.Factory) (*Cluster, error) {
 	q, err := graphql.GetClusterQueues(ctx, f.GraphQLClient, OrganizationSlug, ClusterID)
 	if err != nil {
-		fmt.Println("Unable to read Cluster Queues: ", err.Error())
-		return nil, err
+		return nil, fmt.Errorf("unable to read Cluster Queues: %s", err.Error())
 	}
 
 	ClusterDescription := q.Organization.Cluster.Description
-
 	cluster := Cluster{
 		OrganizationSlug: OrganizationSlug,
 		ClusterID:        ClusterID,
 		Name:             q.Organization.Cluster.Name,
 		Description:      string(*ClusterDescription),
-		Queues:           []Queue{},
+		Queues:           make([]Queue, len(q.Organization.Cluster.Queues.Edges)),
 	}
 
-	for _, edge := range q.Organization.Cluster.Queues.Edges {
-		agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{edge.Node.Id})
-		if err != nil {
-			return nil, fmt.Errorf("unable to read Cluster Queue Agents: %s", err.Error())
-		}
+	eg, ctx := errgroup.WithContext(ctx)
 
-		queue := Queue{
-			Id:           edge.Node.Id,
-			Name:         edge.Node.Key,
-			ActiveAgents: len(agent.Organization.Agents.Edges),
-		}
-		cluster.Queues = append(cluster.Queues, queue)
+	for i, edge := range q.Organization.Cluster.Queues.Edges {
+
+		i, edge := i, edge
+		eg.Go(func() error {
+
+			agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{edge.Node.Id})
+			if err != nil {
+				return fmt.Errorf("unable to read Cluster Queue Agents %s: %s", edge.Node.Id, err.Error())
+			}
+
+			cluster.Queues[i] = Queue{
+				Id:           edge.Node.Id,
+				Name:         edge.Node.Key,
+				ActiveAgents: len(agent.Organization.Agents.Edges),
+			}
+
+			return nil
+		})
 
 	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
 	return &cluster, nil
 }
