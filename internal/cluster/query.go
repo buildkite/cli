670eed3beb0a0b294c61a2e13d616db7e580978b
priya kandababu <priyakandababu@192-168-1-122.tpgi.com.au>
2024-07-12 13:28:52 +1000

    Including errgroup with queue

--- internal/cluster/query.go
+++ internal/cluster/query.go
@@ -3,12 +3,10 @@ package cluster
 import (
 	"context"
 	"fmt"
-	"sync"
-
-	"golang.org/x/sync/errgroup"
 
 	"github.com/buildkite/cli/v3/internal/graphql"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"golang.org/x/sync/errgroup"
 )
 
 func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string, f *factory.Factory) (*Cluster, error) {
@@ -23,46 +21,34 @@ func QueryCluster(ctx context.Context, OrganizationSlug string, ClusterID string
 		ClusterID:        ClusterID,
 		Name:             q.Organization.Cluster.Name,
 		Description:      string(*ClusterDescription),
-		Queues:           []Queue{},
+		Queues:           make([]Queue, len(q.Organization.Cluster.Queues.Edges)),
 	}
 
-	queueChannel := make(chan Queue, len(q.Organization.Cluster.Queues.Edges))
+	eg, ctx := errgroup.WithContext(ctx)
 
-	var wg sync.WaitGroup
-	var e errgroup.Group
+	for i, edge := range q.Organization.Cluster.Queues.Edges {
 
-	for _, edge := range q.Organization.Cluster.Queues.Edges {
-		wg.Add(1)
+		i, edge := i, edge
+		eg.Go(func() error {
 
-		e.Go(func() error {
-			defer wg.Done()
 			agent, err := graphql.GetClusterQueueAgent(ctx, f.GraphQLClient, OrganizationSlug, []string{edge.Node.Id})
 			if err != nil {
 				return fmt.Errorf("unable to read Cluster Queue Agents %s: %s", edge.Node.Id, err.Error())
 			}
 
-			queue := Queue{
+			cluster.Queues[i] = Queue{
 				Id:           edge.Node.Id,
 				Name:         edge.Node.Key,
 				ActiveAgents: len(agent.Organization.Agents.Edges),
 			}
-			queueChannel <- queue
+
 			return nil
-
 		})
+
 	}
 
-	go func() {
-		wg.Wait()
-		close(queueChannel)
-	}()
-
-	for queue := range queueChannel {
-		cluster.Queues = append(cluster.Queues, queue)
-	}
-
-	if err := e.Wait(); err != nil {
-		return nil, fmt.Errorf("error waiting for goroutines: %s", err.Error())
+	if err := eg.Wait(); err != nil {
+		return nil, err
 	}
 
 	return &cluster, nil
