9ef622fe263ff30cc1934bef75aa88dec94072ac
Jarryd Tilbrook <jarryd@buildkite.com>
2024-08-21 12:53:26 +0800

    Split rendering to functions

--- internal/pipeline/view.go
+++ internal/pipeline/view.go
@@ -21,10 +21,49 @@ func RenderPipeline(out writer, p graphql.GetPipelinePipeline) error {
 	if p.Color != nil {
 		hr = hr.BorderForeground(lipgloss.Color(*p.Color))
 	}
-	italic := lipgloss.NewStyle().Italic(true)
-	bold := lipgloss.NewStyle().Bold(true)
 
 	var header, tags, metrics, steps strings.Builder
+
+	err := renderHeader(&header, &p)
+	if err != nil {
+		return err
+	}
+	err = renderTags(&tags, &p)
+	if err != nil {
+		return err
+	}
+	err = renderMetrics(&metrics, &p)
+	if err != nil {
+		return err
+	}
+	err = renderSteps(&steps, &p)
+	if err != nil {
+		return err
+	}
+
+	// find the longest section to use as the overall width
+	w := lipgloss.Width(header.String())
+	for _, section := range []string{tags.String(), metrics.String(), steps.String()} {
+		if current := lipgloss.Width(section); current > w {
+			w = current
+		}
+	}
+
+	fmt.Fprintf(out, "%s", lipgloss.JoinVertical(
+		lipgloss.Center,
+		// add in 4 extra spaces to the header to accommodate the indentation of the steps
+		hr.Width(w+4).AlignHorizontal(lipgloss.Center).Render(header.String()),
+		tags.String(),
+		metrics.String(),
+	))
+	fmt.Fprintf(out, "%s", steps.String())
+
+	return nil
+}
+
+func renderHeader(header writer, p *graphql.GetPipelinePipeline) error {
+	italic := lipgloss.NewStyle().Italic(true)
+	bold := lipgloss.NewStyle().Bold(true)
 
 	if p.Emoji != nil {
 		header.WriteString(*p.Emoji)
@@ -39,7 +78,10 @@ func RenderPipeline(out writer, p graphql.GetPipelinePipeline) error {
 	if p.Favorite {
 		header.WriteString(" ⭐")
 	}
+	return nil
+}
 
+func renderTags(tags writer, p *graphql.GetPipelinePipeline) error {
 	if numTags := len(p.Tags); numTags > 0 {
 		for i, tag := range p.Tags {
 			tags.WriteString(tag.Label)
@@ -49,6 +91,10 @@ func RenderPipeline(out writer, p graphql.GetPipelinePipeline) error {
 		}
 	}
 
+	return nil
+}
+
+func renderMetrics(metrics writer, p *graphql.GetPipelinePipeline) error {
 	if p.Metrics != nil && len(p.Metrics.Edges) > 0 {
 		for i, metric := range p.Metrics.Edges {
 			if metric != nil && metric.Node != nil && metric.Node.Value != nil {
@@ -66,30 +112,14 @@ func RenderPipeline(out writer, p graphql.GetPipelinePipeline) error {
 			}
 		}
 	}
+	return nil
+}
 
-	// find the longest section to use as the overall width
-
+func renderSteps(steps writer, p *graphql.GetPipelinePipeline) error {
 	if p.Steps != nil && p.Steps.Yaml != nil {
 		render, _ := glamour.NewTermRenderer(glamour.WithAutoStyle(), glamour.WithEmoji(), glamour.WithWordWrap(0))
 		r, _ := render.Render(fmt.Sprintf("```yaml\n%s\n```\n", *p.Steps.Yaml))
 		steps.WriteString(r)
 	}
-
-	w := lipgloss.Width(header.String())
-	for _, section := range []string{tags.String(), metrics.String(), steps.String()} {
-		if current := lipgloss.Width(section); current > w {
-			w = current
-		}
-	}
-
-	fmt.Fprintf(out, "%s", lipgloss.JoinVertical(
-		lipgloss.Center,
-		// add in 4 extra spaces to the header to accommodate the indentation of the steps
-		hr.Width(w+4).AlignHorizontal(lipgloss.Center).Render(header.String()),
-		tags.String(),
-		metrics.String(),
-	))
-	fmt.Fprintf(out, "%s", steps.String())
-
 	return nil
 }
