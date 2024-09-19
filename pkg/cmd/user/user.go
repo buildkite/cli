478a3c5ba6688a0adec75788997a18ed69aaae8c
Ben McNicholl <git@benmcnicholl.com>
2024-09-19 15:55:19 +1000

    Remove RunE from base User command so help prints.

--- pkg/cmd/user/user.go
+++ pkg/cmd/user/user.go
@@ -3,22 +3,21 @@ package user
 import (
 	"github.com/MakeNowJust/heredoc"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/buildkite/cli/v3/pkg/cmd/validation"
 	"github.com/spf13/cobra"
 )
 
 func CommandUser(f *factory.Factory) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "user <command>",
-		Short: "Manage users.",
+		Short: "Invite users to the organization",
 		Long: heredoc.Doc(`
       Manage organization users via the CLI.
 
       To invite a user:
       bk user invite [email address]
       `),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
-		},
+		PersistentPreRunE: validation.CheckValidConfiguration(f.Config),
 	}
 
 	cmd.AddCommand(CommandUserInvite(f))
