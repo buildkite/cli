fb1792df033179a702295d9fa6034457c581bd7d
Ben McNicholl <git@benmcnicholl.com>
2025-02-13 15:32:38 +1100

    🐛 Lol I removed the examples

--- pkg/cmd/build/view.go
+++ pkg/cmd/build/view.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 
+	"github.com/MakeNowJust/heredoc"
 	buildResolver "github.com/buildkite/cli/v3/internal/build/resolver"
 	"github.com/buildkite/cli/v3/internal/build/resolver/options"
 	"github.com/buildkite/cli/v3/internal/build/view"
@@ -25,6 +26,29 @@ func NewCmdBuildView(f *factory.Factory) *cobra.Command {
 		Use:                   "view [number] [flags]",
 		Short:                 "View build information",
 		Args:                  cobra.MaximumNArgs(1),
+		Long: heredoc.Doc(`
+					View a build's information.
+					You can pass an optional build number to view. If omitted, the most recent build on the current branch will be resolved.
+				`),
+		Example: heredoc.Doc(`
+					# by default, the most recent build for the current branch is shown
+					$ bk build view
+					# if not inside a repository or to use a specific pipeline, pass -p
+					$ bk build view -p monolith
+					# to view a specific build
+					$ bk build view 429
+					# add -w to any command to open the build in your web browser instead
+					$ bk build view -w 429
+					# to view the most recent build on feature-x branch
+					$ bk build view -b feature-y
+					# you can filter by a user name or id
+					$ bk build view -u "alice"
+					# a shortcut to view your builds is --mine
+					$ bk build view --mine
+					# you can combine most of these flags
+					# to view most recent build by greg on the deploy-pipeline
+					$ bk build view -p deploy-pipeline -u "greg"
+				`),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// Resolve pipeline first
 			pipelineRes := pipelineResolver.NewAggregateResolver(
