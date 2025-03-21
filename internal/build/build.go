bba2be529ba4c2c547e2e274a089789782cb746e
Ben McNicholl <git@benmcnicholl.com>
2025-03-21 13:04:41 +1100

    🎨 Use our new models in our build and pipeline files

--- internal/build/build.go
+++ internal/build/build.go
@@ -1,7 +1,32 @@
 package build
 
+import (
+	"github.com/buildkite/cli/v3/internal/models"
+)
+
+// Build represents a Buildkite build
+// For backward compatibility, we're keeping the same field names as before
+// but the underlying type is models.Build
 type Build struct {
 	Organization string
 	Pipeline     string
 	BuildNumber  int
+}
+
+// ToModel converts the Build to a models.Build
+func (b *Build) ToModel() *models.Build {
+	return &models.Build{
+		Organization: b.Organization,
+		Pipeline:     b.Pipeline,
+		BuildNumber:  b.BuildNumber,
+	}
+}
+
+// FromModel creates a Build from a models.Build
+func FromModel(mb *models.Build) *Build {
+	return &Build{
+		Organization: mb.Organization,
+		Pipeline:     mb.Pipeline,
+		BuildNumber:  mb.BuildNumber,
+	}
 }
