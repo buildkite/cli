60cfbf128ac30048095dde0925729a1aeeb7ddc9
Ben Moskovitz <ben@mosk.nz>
2025-04-29 16:32:18 +1000

    Split errors for empty API token and empty org slug (#475)
    Previously, if the org slug was missing, it would output an error saying that the API token was missing, which wasn't necessarily the case. This commit splits them into multiple errors, and also adds a recommendation to set environment variables as a workaround.

--- pkg/cmd/validation/config.go
+++ pkg/cmd/validation/config.go
@@ -39,10 +39,12 @@ func getCommandPath(cmd *cobra.Command) string {
 // CheckValidConfiguration returns a function that checks the viper configuration is valid to execute the command
 func CheckValidConfiguration(conf *config.Config) func(cmd *cobra.Command, args []string) error {
 	var err error
+	if conf.APIToken() == "" {
+		err = errors.New("you must set a valid API token. run `bk configure`, or set the environment variable `BUILDKITE_API_TOKEN`")
+	}
 
-	// ensure the configuration has an API token set
-	if conf.APIToken() == "" || conf.OrganizationSlug() == "" {
-		err = errors.New("you must set a valid API token. run `bk configure`")
+	if conf.OrganizationSlug() == "" {
+		err = errors.New("you must set a valid organization slug. run `bk configure`, or set the environment variable `BUILDKITE_ORGANIZATION_SLUG`")
 	}
 
 	return func(cmd *cobra.Command, args []string) error {
