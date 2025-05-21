edce0c57a83310ca4e344c0bd64f8883ccf59338
Ben McNicholl <git@benmcnicholl.com>
2025-05-21 20:56:30 +1000

    chore: remove bubbletea from configure
    Signed-off-by: Ben McNicholl <git@benmcnicholl.com>

--- pkg/cmd/configure/add/add.go
+++ pkg/cmd/configure/add/add.go
@@ -1,11 +1,16 @@
 package add
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
 
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
-	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 func NewCmdAdd(f *factory.Factory) *cobra.Command {
@@ -30,25 +35,49 @@ func ConfigureWithCredentials(f *factory.Factory, org, token string) error {
 }
 
 func ConfigureRun(f *factory.Factory) error {
-	var org, token string
-	nonEmpty := func(s string) error {
-		if len(s) == 0 {
-			return errors.New("value cannot be empty")
-		}
-		return nil
-	}
-	form := huh.NewForm(
-		huh.NewGroup(
-			huh.NewInput().Title("Organization slug: ").Value(&org).Validate(nonEmpty).Inline(true).Prompt(""),
-		),
-		huh.NewGroup(
-			huh.NewInput().Title("API Token: ").Value(&token).EchoMode(huh.EchoModePassword).Validate(nonEmpty).Inline(true).Prompt(""),
-		),
-	).WithTheme(huh.ThemeBase16())
-	err := form.Run()
+	// Get organization slug
+	org, err := promptForInput("Organization slug: ", false)
 	if err != nil {
 		return err
 	}
+	if org == "" {
+		return errors.New("organization slug cannot be empty")
+	}
+
+	// Get API token with password input (no echo)
+	token, err := promptForInput("API Token: ", true)
+	if err != nil {
+		return err
+	}
+	if token == "" {
+		return errors.New("API token cannot be empty")
+	}
+
+	fmt.Println("API token set for organization:", org)
 
 	return ConfigureWithCredentials(f, org, token)
+}
+
+// promptForInput handles terminal input with optional password masking
+func promptForInput(prompt string, isPassword bool) (string, error) {
+	fmt.Print(prompt)
+
+	if isPassword {
+		// Use term.ReadPassword for secure password input
+		passwordBytes, err := term.ReadPassword(int(syscall.Stdin))
+		fmt.Println() // Add a newline after password input
+		if err != nil {
+			return "", err
+		}
+		return string(passwordBytes), nil
+	} else {
+		// Use standard input for regular text
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+		// Trim whitespace and newlines
+		return strings.TrimSpace(input), nil
+	}
 }
