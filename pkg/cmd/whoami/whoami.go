34a24ffd0b6971dd69e45d308302ad2acb0ca516
Ben Moskovitz <ben.m@buildkite.com>
2025-07-23 10:28:11 +1000

    Use output module for output formatting

--- pkg/cmd/whoami/whoami.go
+++ pkg/cmd/whoami/whoami.go
@@ -1,30 +1,43 @@
 package whoami
 
 import (
-	"encoding/json"
 	"fmt"
-	"slices"
 	"strings"
 
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
+	"github.com/buildkite/cli/v3/pkg/output"
 	"github.com/buildkite/go-buildkite/v4"
 	"github.com/spf13/cobra"
 )
 
-const (
-	formatPlain = "plain"
-	formatJSON  = "json"
-)
+type whoamiOutput struct {
+	OrganizationSlug string                `json:"organization_slug"`
+	Token            buildkite.AccessToken `json:"token"`
+}
+
+func (w whoamiOutput) TextOutput() string {
+	b := strings.Builder{}
+
+	b.WriteString(fmt.Sprintf("Current organization: %s\n", w.OrganizationSlug))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("API Token UUID:        %s\n", w.Token.UUID))
+	b.WriteString(fmt.Sprintf("API Token Description: %s\n", w.Token.Description))
+	b.WriteString(fmt.Sprintf("API Token Scopes:      %v\n", w.Token.Scopes))
+	b.WriteRune('\n')
+	b.WriteString(fmt.Sprintf("API Token user name:  %s\n", w.Token.User.Name))
+	b.WriteString(fmt.Sprintf("API Token user email: %s\n", w.Token.User.Email))
+
+	return b.String()
+}
 
 func NewCmdWhoami(f *factory.Factory) *cobra.Command {
-	output := "plain"
-
 	cmd := &cobra.Command{
 		Use:   "whoami",
 		Short: "Print the current user and organization",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if !slices.Contains([]string{formatPlain, formatJSON}, output) {
-				return fmt.Errorf("invalid output: %s, must be one of: %s, %s", output, formatPlain, formatJSON)
+			format, err := output.GetFormat(cmd.Flags())
+			if err != nil {
+				return fmt.Errorf("failed to get output format: %w", err)
 			}
 
 			orgSlug := f.Config.OrganizationSlug()
@@ -37,41 +50,20 @@ func NewCmdWhoami(f *factory.Factory) *cobra.Command {
 				return fmt.Errorf("failed to get access token: %w", err)
 			}
 
-			if output == formatJSON {
-				type whoami struct {
-					OrganizationSlug string                `json:"organization_slug"`
-					Token            buildkite.AccessToken `json:"token"`
-				}
-
-				whoamiInfo := whoami{
-					OrganizationSlug: orgSlug,
-					Token:            token,
-				}
-
-				err := json.NewEncoder(cmd.OutOrStdout()).Encode(whoamiInfo)
-				if err != nil {
-					return fmt.Errorf("failed to encode whoami info to JSON: %w", err)
-				}
-
-				return nil
+			w := whoamiOutput{
+				OrganizationSlug: orgSlug,
+				Token:            token,
 			}
 
-			b := strings.Builder{}
-
-			b.WriteString(fmt.Sprintf("Current organization: %s\n", orgSlug))
-			b.WriteRune('\n')
-			b.WriteString(fmt.Sprintf("API Token Description: %s\n", token.Description))
-			b.WriteString(fmt.Sprintf("API Token Scopes: %v\n", token.Scopes))
-			b.WriteRune('\n')
-			b.WriteString(fmt.Sprintf("API Token user name: %s\n", token.User.Name))
-			b.WriteString(fmt.Sprintf("API Token user email: %s\n", token.User.Email))
-
-			fmt.Fprint(cmd.OutOrStdout(), b.String())
+			err = output.Write(cmd.OutOrStdout(), w, format)
+			if err != nil {
+				return fmt.Errorf("failed to write output: %w", err)
+			}
 
 			return nil
 		},
 	}
 
-	cmd.Flags().StringVarP(&output, "output", "o", output, "Output format (plain (default) or json)")
+	output.AddFlags(cmd.Flags())
 	return cmd
 }
