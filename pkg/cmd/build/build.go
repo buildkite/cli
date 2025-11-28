7505168d3450fa7c60a29d862106e5b3d6d86839
Joe Coleman <joe@buildkite.com>
2025-11-28 15:25:01 +0000

    Remove old reference to new.go

--- pkg/cmd/build/build.go
+++ pkg/cmd/build/build.go
@@ -32,7 +32,6 @@ func NewCmdBuild(f *factory.Factory) *cobra.Command {
 	cmd.AddCommand(NewCmdBuildCancel(f))
 	cmd.AddCommand(NewCmdBuildDownload(f))
 	cmd.AddCommand(NewCmdBuildList(f))
-	cmd.AddCommand(NewCmdBuildNew(f))
 	cmd.AddCommand(NewCmdBuildRebuild(f))
 	cmd.AddCommand(NewCmdBuildView(f))
 	cmd.AddCommand(NewCmdBuildWatch(f))
