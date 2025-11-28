64c6d6738df37a8b7a0b0e234e097a238865e735
Joe Coleman <joe@buildkite.com>
2025-11-28 16:24:55 +0000

    [skip ci] Removing list subcommand from build.go

--- pkg/cmd/build/build.go
+++ pkg/cmd/build/build.go
@@ -30,7 +30,6 @@ func NewCmdBuild(f *factory.Factory) *cobra.Command {
 	cmd.PersistentFlags().StringP("pipeline", "p", "", "The pipeline to use. This can be a {pipeline slug} or in the format {org slug}/{pipeline slug}.")
 
 	cmd.AddCommand(NewCmdBuildDownload(f))
-	cmd.AddCommand(NewCmdBuildList(f))
 	cmd.AddCommand(NewCmdBuildRebuild(f))
 	cmd.AddCommand(NewCmdBuildWatch(f))
 
