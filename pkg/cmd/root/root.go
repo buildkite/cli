e80c08c39eb784c400f77c584e0ff6b0c1794344
Joe Coleman <joe@buildkite.com>
2025-12-15 17:17:02 +0000

    Cleanup remnants of init

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -6,7 +6,6 @@ import (
 	"github.com/MakeNowJust/heredoc"
 	configureCmd "github.com/buildkite/cli/v3/pkg/cmd/configure"
 	"github.com/buildkite/cli/v3/pkg/cmd/factory"
-	initCmd "github.com/buildkite/cli/v3/pkg/cmd/init"
 	packageCmd "github.com/buildkite/cli/v3/pkg/cmd/pkg"
 	promptCmd "github.com/buildkite/cli/v3/pkg/cmd/prompt"
 	"github.com/spf13/cobra"
@@ -43,7 +42,6 @@ func NewCmdRoot(f *factory.Factory) (*cobra.Command, error) {
 	}
 
 	cmd.AddCommand(configureCmd.NewCmdConfigure(f))
-	cmd.AddCommand(initCmd.NewCmdInit(f))
 	cmd.AddCommand(packageCmd.NewCmdPackage(f))
 	cmd.AddCommand(promptCmd.NewCmdPrompt(f))
 
