4c2089b31573a590455d8e81823e9ad02071b2bc
Joe Coleman <joe@buildkite.com>
2025-12-29 15:50:48 +0000

    Migrate pipeline view to new table view and pager
    Adds global check for no pager
    Updates color checks to ensure isatty or dumb term now too

--- pkg/output/color.go
+++ pkg/output/color.go
@@ -3,6 +3,8 @@ package output
 import (
 	"os"
 	"sync"
+
+	"github.com/mattn/go-isatty"
 )
 
 var (
@@ -16,6 +18,17 @@ func ColorEnabled() bool {
 	colorOnce.Do(func() {
 		if _, disabled := os.LookupEnv("NO_COLOR"); disabled {
 			colorEnabled = false
+			return
+		}
+
+		if term := os.Getenv("TERM"); term == "dumb" {
+			colorEnabled = false
+			return
+		}
+
+		if !(isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())) {
+			colorEnabled = false
+			return
 		}
 	})
 	return colorEnabled
