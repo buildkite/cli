4ca8a6fa0a8e99cc82f28477f0f4805741176ee7
Ben McNicholl <ben.mcnicholl@buildkite.com>
2026-02-11 16:13:35 +1100

    Publish to Docker. Only build 21st century CPUs.
    Releasing to Docker Hub would be a nice QOL improvement for folks who need to run the CLI via Docker, and don't want to clone down `main` every time.
    
    I've also removed 32 bit architectures from build targets, because it's 2026 not 1976.

--- .buildkite/release.sh
+++ .buildkite/release.sh
@@ -14,6 +14,18 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+# check if DOCKERHUB_USER and DOCKERHUB_PASSWORD are set if not skip docker login
+if [[ -z "${DOCKERHUB_USER:-}" || -z "${DOCKERHUB_PASSWORD:-}" ]]; then
+    echo "Skipping Docker login as DOCKERHUB_USER or DOCKERHUB_PASSWORD is not set"
+else
+    echo "--- :key: :docker: Login to Docker Hub using ko"
+    echo "${DOCKERHUB_PASSWORD}" | ko login index.docker.io --username "${DOCKERHUB_USER}" --password-stdin
+    if [[ $? -ne 0 ]]; then
+        echo "Docker login failed"
+        exit 1
+    fi
+fi
+
 if ! goreleaser "$@"; then
     echo "Failed to build a release"
     exit 1
