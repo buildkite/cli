c80f31e4f46085ca42b92ad8c88ef5a15645dc9e
Thomas Desrosiers <git@hive.pw>
2026-03-31 17:10:09 -0700

    test: add failing tests for unblock response validation

---
+++ cmd/job/unblock_test.go
@@ -0,0 +1,72 @@
+package job
+
+import (
+	"testing"
+
+	bkGraphQL "github.com/buildkite/cli/v3/internal/graphql"
+)
+
+func TestValidateUnblockResponse(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		input   *bkGraphQL.UnblockJobResponse
+		wantErr bool
+		errMsg  string
+	}{
+		{
+			name:    "nil response",
+			input:   nil,
+			wantErr: true,
+			errMsg:  "failed to unblock job",
+		},
+		{
+			name: "nil payload",
+			input: &bkGraphQL.UnblockJobResponse{
+				JobTypeBlockUnblock: nil,
+			},
+			wantErr: true,
+			errMsg:  "failed to unblock job",
+		},
+		{
+			name: "successful unblock",
+			input: &bkGraphQL.UnblockJobResponse{
+				JobTypeBlockUnblock: &bkGraphQL.UnblockJobJobTypeBlockUnblockJobTypeBlockUnblockPayload{
+					JobTypeBlock: bkGraphQL.UnblockJobJobTypeBlockUnblockJobTypeBlockUnblockPayloadJobTypeBlock{
+						State: bkGraphQL.JobStatesUnblocked,
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "unexpected state BLOCKED",
+			input: &bkGraphQL.UnblockJobResponse{
+				JobTypeBlockUnblock: &bkGraphQL.UnblockJobJobTypeBlockUnblockJobTypeBlockUnblockPayload{
+					JobTypeBlock: bkGraphQL.UnblockJobJobTypeBlockUnblockJobTypeBlockUnblockPayloadJobTypeBlock{
+						State: bkGraphQL.JobStatesBlocked,
+					},
+				},
+			},
+			wantErr: true,
+			errMsg:  "BLOCKED",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := validateUnblockResponse(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("validateUnblockResponse() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.wantErr && err != nil && tt.errMsg != "" {
+				// Check that the error message contains the expected substring
+				if err.Error() == "" || (tt.errMsg != "" && len(err.Error()) > 0) {
+					// Just verify error exists if we expected one
+					return
+				}
+			}
+		})
+	}
+}
